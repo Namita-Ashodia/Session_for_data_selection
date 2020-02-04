95ef59ea1860c20c0f8e685b4d46d386abba7b31
Namita Ashodia <58926975+Namita-Ashodia@users.noreply.github.com>
2020-02-04 20:32:23 +0530

    Add files via upload

---
+++ pricing.php
@@ -0,0 +1,310 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="keywords" content="Digital marketing agency, Digital marketing company, Digital marketing services">
+		<meta name="author" content="creativegigs">
+		<meta name="description" content="rogan creative multipurpose is a beautiful website template designed for SEO & Digital Agency websites.">
+		<meta name='og:image' content='images/home/ogg.png'>
+		<!-- For IE -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<!-- For Resposive Device -->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!-- For Window Tab Color -->
+		<!-- Chrome, Firefox OS and Opera -->
+		<meta name="theme-color" content="#fff">
+		<!-- Windows Phone -->
+		<meta name="msapplication-navbutton-color" content="#fff">
+		<!-- iOS Safari -->
+		<meta name="apple-mobile-web-app-status-bar-style" content="#fff">
+		<title>Rogan - Business and Agency html Template</title>
+		<!-- Favicon -->
+		<link rel="icon" type="image/png" sizes="56x56" href="images/fav-icon/icon.png">
+		<!-- Main style sheet -->
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<!-- responsive style sheet -->
+		<link rel="stylesheet" type="text/css" href="css/responsive.css">
+
+		<!-- Fix Internet Explorer ______________________________________-->
+		<!--[if lt IE 9]>
+			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+			<script src="vendor/html5shiv.js"></script>
+			<script src="vendor/respond.js"></script>
+		<![endif]-->	
+	</head>
+
+	<body>
+		<div class="main-page-wrapper">
+
+			<!-- ===================================================
+				Loading Transition
+			==================================================== -->
+			<!-- Preloader -->
+			
+
+
+			<!-- 
+			=============================================
+				Theme Main Menu
+			============================================== 
+			-->
+				<!-- Preloader -->
+				<section>
+				<div id="preloader">
+					<div id="ctn-preloader" class="ctn-preloader">
+						<div class="animation-preloader">
+							<div class="icon"><img src="images/1.svg" alt=""></div>
+							<div class="txt-loading">
+								<span data-text-preloader="F" class="letters-loading">
+									F
+								</span>
+								<span data-text-preloader="U" class="letters-loading">
+									U
+								</span>
+								<span data-text-preloader="E" class="letters-loading">
+									E
+								</span>
+								<span data-text-preloader="R" class="letters-loading">
+									R
+								</span>
+								<span data-text-preloader="T" class="letters-loading">
+									T
+								</span>
+								<span data-text-preloader="E" class="letters-loading">
+									E
+								</span>
+								<span data-text-preloader="N" class="letters-loading">
+									N
+								</span>
+								<span data-text-preloader="E" class="letters-loading">
+									E
+								</span>
+								<span data-text-preloader="T" class="letters-loading">
+									T
+								</span>
+								<span data-text-preloader="W" class="letters-loading">
+									W
+								</span>
+								<span data-text-preloader="O" class="letters-loading">
+									O
+								</span>
+								<span data-text-preloader="R" class="letters-loading">
+									R
+								</span>
+								<span data-text-preloader="K" class="letters-loading">
+									K
+								</span>
+							</div>
+						</div>	
+					</div>
+				</div>
+			</section>
+
+
+			<!-- 
+			=============================================
+				Theme Main Menu
+			============================================== 
+			-->
+			<?php 
+			include "header.php"
+			?>
+ <!-- /.theme-main-menu -->
+
+
+
+
+			<!-- 
+			=============================================
+				Text Inner Banner One
+			============================================== 
+			-->
+		 <!-- /.text-inner-banner-one -->
+
+			
+			
+			<!-- Element Style -->
+			
+
+
+
+
+
+			<!-- Element Style -->
+			
+
+
+
+
+			<div class="element-section mb-150">
+				<div class="pricing-plan-three">
+					<div class="container">
+						<div class="theme-title-one text-center pb-45">
+							<div class="upper-title mb-5">Pricing Plan</div>
+							<h2 class="main-title">No Hidden Charges! Choose<br><i>your Plan.</i></h2>
+						</div> <!-- /.theme-title-one -->
+
+						<div class="row">
+							<div class="col-lg-4" data-aos="fade-up">
+								<div class="single-pr-table subway">
+									<div class="pr-header">
+										<h4 class="title">Subway</h4>
+										<div class="price">$39.<sup>99</sup></div>
+										<div class="package">Weekly</div>
+									</div> <!-- /.pr-header -->
+									<div class="pr-body">
+										<div class="slogan">Quick job, Small Work</div>
+										<ul>
+											<li>50GB Bandwidth</li>
+											<li>Business & Financ Analysing</li>
+											<li>24 hour support</li>
+											<li>Customer Managemet</li>
+										</ul>
+									</div> <!-- /.pr-body -->
+									<div class="pr-footer">
+										<a href="#" class="plan-button theme-btn line-button-two">Choose Plan</a>
+										<a href="#" class="trial-button">Get your 30 day free trial</a>
+									</div> <!-- /.pr-footer -->
+								</div> <!-- /.single-pr-table -->
+							</div> <!-- /.col- -->
+
+							<div class="col-lg-4" data-aos="fade-up" data-aos-delay="300">
+								<div class="single-pr-table sandwich">
+									<div class="pr-header">
+										<h4 class="title">Sandwich</h4>
+										<div class="price">$99.<sup>99</sup></div>
+										<div class="package">Monthly</div>
+									</div> <!-- /.pr-header -->
+									<div class="pr-body">
+										<div class="slogan">Bigger, Great for Team</div>
+										<ul>
+											<li>50GB Bandwidth</li>
+											<li>Business & Financ Analysing</li>
+											<li>24 hour support</li>
+											<li>Customer Managemet</li>
+										</ul>
+									</div> <!-- /.pr-body -->
+									<div class="pr-footer">
+										<a href="#" class="plan-button theme-btn line-button-two">Choose Plan</a>
+										<a href="#" class="trial-button">Get your 30 day free trial</a>
+									</div> <!-- /.pr-footer -->
+								</div> <!-- /.single-pr-table -->
+							</div> <!-- /.col- -->
+
+							<div class="col-lg-4" data-aos="fade-up" data-aos-delay="500">
+								<div class="single-pr-table burger">
+									<div class="pr-header">
+										<h4 class="title">Burger</h4>
+										<div class="price">$989.<sup>99</sup></div>
+										<div class="package">yealry</div>
+									</div> <!-- /.pr-header -->
+									<div class="pr-body">
+										<div class="slogan"> Great for Corporation</div>
+										<ul>
+											<li>50GB Bandwidth</li>
+											<li>Business & Financ Analysing</li>
+											<li>24 hour support</li>
+											<li>Customer Managemet</li>
+										</ul>
+									</div> <!-- /.pr-body -->
+									<div class="pr-footer">
+										<a href="#" class="plan-button theme-btn line-button-two">Choose Plan</a>
+										<a href="#" class="trial-button">Get your 30 day free trial</a>
+									</div> <!-- /.pr-footer -->
+								</div> <!-- /.single-pr-table -->
+							</div> <!-- /.col- -->
+						</div> <!-- /.row -->
+					</div> <!-- /.container -->
+				</div> <!-- /.pricing-plan-three -->
+			</div>
+			
+
+
+			<!--
+			=====================================================
+				Footer
+			=====================================================
+			-->
+			<footer class="theme-footer-one top-border main-p-color pt-130">
+				<div class="top-footer">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-3 col-sm-6 col-12 footer-about-widget">
+								<a href="index.html" class="logo"><img src="images/logo/logo.svg" alt=""></a>
+								<a href="#" class="email">rogan.inc@company.com</a>
+								<a href="#" class="phone">720.661.2231</a>
+								
+							</div> <!-- /.about-widget -->
+							<div class="col-lg-3 col-lg-3 col-sm-6 col-12 footer-list">
+								<h5 class="footer-title">Services</h5>
+								<ul>
+									<li><a href="#">Web Design</a></li>
+									<li><a href="#">Development</a></li>
+									<li><a href="#">Wordpress</a></li>
+									<li><a href="#">Online Marketing</a></li>
+									<li><a href="#">Content</a></li>
+								</ul>
+							</div> <!-- /.footer-recent-post -->
+							<div class="col-lg-3 col-sm-6 col-12 footer-list">
+								<h5 class="footer-title">About us</h5>
+								<ul>
+									<li><a href="about-standard.html">About us</a></li>
+									<li><a href="portfolio-v1.html">Work Portfolio</a></li>
+									<li><a href="team-v1.html">Team</a></li>
+									<li><a href="#">Plan & Pricing</a></li>
+									<li><a href="blog-v1.html">News</a></li>
+								</ul>
+							</div> <!-- /.footer-list -->
+							<div class="col-lg-3 col-lg-2 col-sm-6 col-12 footer-information">
+								<h5 class="footer-title">Our Address</h5>
+								<p>00 Orville Road Apt. 728 <br>California, USA</p>
+								<ul>
+									<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+									<li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+								</ul>
+							</div>
+						</div> <!-- /.row -->
+					</div> <!-- /.container -->
+				</div> <!-- /.top-footer -->
+				
+				<div class="container">
+					<div class="bottom-footer-content">
+						<p>&copy; 2019 copyright all right reserved</p>
+					</div> <!-- /.bottom-footer -->
+				</div>
+			</footer> <!-- /.theme-footer-one -->
+			
+
+	        
+
+	        <!-- Scroll Top Button -->
+			<button class="scroll-top tran3s">
+				<i class="fa fa-angle-up" aria-hidden="true"></i>
+			</button>
+			
+
+
+		<!-- Optional JavaScript _____________________________  -->
+
+    	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    	<!-- jQuery -->
+		<script src="vendor/jquery.2.2.3.min.js"></script>
+		<!-- Popper js -->
+		<script src="vendor/popper.js/popper.min.js"></script>
+		<!-- Bootstrap JS -->
+		<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+	    <!-- menu  -->
+		<script src="vendor/mega-menu/assets/js/custom.js"></script>
+		<!-- AOS js -->
+		<script src="vendor/aos-next/dist/aos.js"></script>
+		<!-- WOW js -->
+		<script src="vendor/WOW-master/dist/wow.min.js"></script>
+		<!-- owl.carousel -->
+		<script src="vendor/owl-carousel/owl.carousel.min.js"></script>
+
+		<!-- Theme js -->
+		<script src="js/theme.js"></script>
+		</div> <!-- /.main-page-wrapper -->
+	</body>
+</html>
